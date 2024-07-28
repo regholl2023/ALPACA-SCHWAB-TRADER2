17401cd03b72653c9e6fd5db6b3c9c5e33567eeb
TheLeetTaco <tboch3@gmail.com>
2024-07-28 13:14:09 -0400

    More code layout and comments

--- copy_trade.py
+++ copy_trade.py
@@ -1,21 +1,70 @@
-from alpaca_api import check_for_change
+from schwab_api import breakdown_account_by_quotes, submit_orders, is_token_valid
+from alpaca_api import check_for_change, get_alpaca_percentages
 from logger_config import setup_logger
 from dotenv import load_dotenv
 import time
+import os
 
 logger = setup_logger('copy_trade', 'logs/copy_trade.log')
 
 # Load environment variables from .env file
 load_dotenv()
 
-# Copy Trade Variables
-PERIOD=1
+# --------------------------------------
+# How often we want to check for changes
+PERIOD = 1
+# --------------------------------------
 
-# Execute the script
+def execute_trades_across_accounts(percentages: dict) -> bool:
+    """Will execute trades across all selected accounts
+
+    Args:
+        percentages (dict): Alpaca percentages
+
+    Returns:
+        bool: True if Executed successfully, False if not
+    """
+    
+    accounts = os.getenv('SCHWAB_ACCOUNT_NUMS')
+    
+    # Let split out the accounts or put a single one into a list
+    if "," in accounts:
+        accounts = accounts.split(",")
+    else:
+        accounts = [accounts]
+        
+    # Iterate over all the accounts we selected and execute trades
+    for account in accounts:
+        pass
+    
+    return False
+
+# How we run the copy trade method
 if __name__ == '__main__':
     while True:
         time.sleep(PERIOD)
         
+        if is_token_valid is False:
+            # If the token is invalid lets get into an infinite loop of notifications
+            while True:
+                logger.error("\n!!! Invalid Token !!!\n")
+                # Lets send an Email/SMS that the token is Invalid 
+                pass
+            
         # We are just going to check for changes and then copy trade instead of
         if check_for_change():
-            print("Change Occured!")
+            logger.info("\n=== Change Detected ===\n")
+            # Either sleep for a period or we check that all orders on Alpaca are done
+            output = execute_trades_across_accounts(get_alpaca_percentages())
+            
+            if output is True:
+                logger.info("\n=== Trading Completed Successfully ===\n")
+                # Nothing else to do lets go back to checking for changes
+                continue
+            else:
+                # Lets get into a lock so we dont trade anyfurther
+                while True:
+                    logger.error("\n!!! Trading FAILED !!!\n")
+                    # Lets send an Email/SMS that a trade failure occured 
+                    pass
+            
--- schwab_api.py
+++ schwab_api.py
@@ -1,8 +1,8 @@
 from schwab.auth import client_from_manual_flow
 from logger_config import setup_logger
 from dotenv import load_dotenv
-from enum import Enum
 import schwab.client as Client
+from enum import Enum
 
 # Load environment variables from .env file
 load_dotenv()
@@ -19,6 +19,14 @@ class Modes(Enum):
 # --------------------------------------
 # Used to dictate if orders are real or not
 MODE = Modes.DEBUG
+# Percentage of account equity to trade with (i.e. 1%-100%)
+TRADE_WITH = 100
+# In the above example, using 90% with a 500$ padding:
+#  Account equity = $100,000 * .90 = $90,000 - $500 = $89,500.  
+#  This is the amount that will be used as the starting point for all calculations when determining how many shares to purchase, etc.  
+#  This allows for some slippage, etc.
+# Minimum value for padding is $500
+PADDING = 500
 # --------------------------------------
 
 def is_token_valid() -> bool:
@@ -35,7 +43,7 @@ def get_quotes(symbols: list):
     Args:
         symbols (list): List of Symbols to get quotes for
     """
-    Client.get_quotes()
+    # Client.get_quotes()
     pass
 
 def get_account_total_value(account_hash: str):
@@ -44,11 +52,11 @@ def get_account_total_value(account_hash: str):
     Args:
         account_hash (str): hash of account to interact with
     """
-    Client.get_account()
+    # Client.get_account()
     pass
 
 def breakdown_account_by_quotes(account_hash: str, percentages: dict) -> dict:
-    """Used to breakout an accounts value by the percentages from Alpaca
+    """Used to breakout an accounts value by the percentages from Alpaca, TRADE_WITH, and PADDING
 
     Args:
         account_hash (str): hash of account to interact with
@@ -65,7 +73,7 @@ def check_orders(account_hash: str):
     Args:
         account_hash (str): hash of account to interact with
     """
-    Client.get_orders_for_account()
+    # Client.get_orders_for_account()
     pass
 
 def submit_orders(account_hash: str, orders: list):
@@ -79,7 +87,16 @@ def submit_orders(account_hash: str, orders: list):
     if MODE is Modes.DEBUG:
         pass
     elif MODE is Modes.LIVE:
-        Client.place_order()
+        # Client.place_order()
+        # Log the orders submitted for sells
+        # Execute sell orders
+        # Log that we are checking for sell orders
+        # Once we see all sell orders are executed we exit.
+        # Check for open sell orders every 1 second until the API indicates that no outstanding sell orders exist.  
+        # Abort this check after ~5 seconds (in the event that there is an issue with the response from Schwab).
+        # Log the buy orders
+        # Execute buy orders
+        pass
     else:
         pass
 
